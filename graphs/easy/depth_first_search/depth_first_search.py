66eccbedc2162dcfc27faf71e6241edbc2fe56d0
nmandal <nicholas.mandal@gmail.com>
2018-12-20 14:25:24 -0600

    depth first search

---
+++ graphs/easy/depth_first_search/depth_first_search.py
@@ -0,0 +1,14 @@
+# O(V + E) time | O(V) space
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+    
+    def add_child(self, name):
+        self.children.append(Node(name))
+        
+    def depth_first_search(self, array):
+        array.append(self.name)
+        for child in self.children:
+            child.depth_first_search(array)
+        return array
